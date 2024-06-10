25dd1e0613feac67fc7eb2a5c8c945425a6ccc62
AnnaDer888 <an4ik09@yandex.ru>
2024-06-10 20:32:43 +0300

    Загружаем файл

---
+++ src/test/java/TestDollar.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class TestDollar {
+    @Test
+    void MyOne() {
+        open("http://92.51.36.108:7777/top.academy/xpath/v01/");
+        sleep(50000);
+        $("body > form > div:nth-child(1) > input[type=text]").setValue("100");
+        $("[type=submit]").click();
+        $("#sum").shouldHave(text("100"));
+        $(byText("Вы внесли 100 рублей")).shouldBe(exist);
+
+    }
+    @Test
+    void MyTwo() {
+        open("http://92.51.36.108:7777/top.academy/xpath/v01/");
+        sleep(50000);
+        $("body > form > div:nth-child(3) > input[type=text]").setValue("100");
+        $("body > form > input[type=submit]").click();
+        $(byText("Вы внесли 100 долларов")).shouldBe(exist);
+}
+    @Test
+    void MyT() {
+        open("http://92.51.36.108:7777/top.academy/xpath/v01/");
+        sleep(50000);
+        $("body > form > div:nth-child(5) > input[type=text]").setValue("100");
+        $("body > form > input[type=submit]").click();
+        $(byText("Вы внесли 100 евро")).shouldBe(exist);
+
+}
+}
+
+
+
+
+
